e75a02c06ada350eb060e3480b21b2156b0faa83
Fadlan270906 <fadlanfathurrahman123@gmail.com>
2025-02-25 20:32:17 +0700

    kumpulin tugas 2

---
+++ kuliah/pertemuan4/Latihan1.php
@@ -0,0 +1,29 @@
+<?php
+//PENGULANGAN / LOOP / LOOPING 
+//WHILE
+// komponen while:
+// 1. nilai awal / inisialisasi
+// 2. kondisi terminasi / kapan harus berhenti 
+// 3. increment / decrement
+$i = 5;
+while($i >= 1) {
+    echo "Hello World $i x <br>";
+    $i = $i - 1;
+} 
+
+$x = 10;
+echo "$x" ;
+echo " <hr>";
+
+
+
+// for 
+// for(nikai_awal; kondisi_terminasi; increment/dec){
+//aksi
+//}
+for($i = 1; $i <= 10; $i++) {
+    echo "Hello World $i x <br>";
+}
+
+
+
